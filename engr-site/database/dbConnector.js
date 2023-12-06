51ac1e7568e786b0ce66f9ee9d4c42325034a38d
josephBabal <jbabal301@gmail.com>
2023-12-06 15:52:26 -0800

    added backend stuff

---
+++ engr-site/database/dbConnector.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2/promise';
+
+
+const db = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASSWORD,
+  database        : process.env.DB_NAME
+})
+
+async function dbConnect({query, values}) {
+  try {
+    const results = await db.query(query, values)
+    return results
+  } catch (error) {
+    return { error };
+  }
+}
+
+export default dbConnect;
