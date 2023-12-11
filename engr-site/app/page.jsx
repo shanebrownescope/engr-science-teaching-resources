bc8caa717faef0c1c1bfbda2f53cfbc455b6ec1c
John Nguyen <nguyenduyjohn180@gmail.com>
2023-12-10 23:08:16 -0800

    got course page started

---
+++ engr-site/app/page.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CoursePage } from '../components';
+
+export default function Home() {
+  return <CoursePage />;
+}
---
+++ engr-site/components/CoursePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursePage = () => {
+  return (
+    <div>
+      <h1>Course Page</h1>
+    </div>
+  );
+};
+
+export default CoursePage;
