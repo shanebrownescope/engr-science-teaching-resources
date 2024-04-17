03795b4638fc535c09f099faf71ecea25f3aed27
John Nguyen <nguyenduyjohn180@gmail.com>
2024-04-17 01:03:13 -0700

    updated home page

--- engr-site/app/(protected)/home/page.tsx
+++ engr-site/app/(protected)/home/page.tsx
@@ -1,13 +1,51 @@
-//* home page when users first log in
+"use client";
+import "@mantine/core/styles.css";
+import { SearchButton } from "@/components/mantine";
+import Link from "next/link";
+import "./page.css";
 
-import { fetchSearchResults } from "@/actions/fetching/fetchSearchResults"
-
-const Home = async() => { 
-
-
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div
+        className="banner"
+        style={{
+          width: "100%",
+          height: "400px",
+          backgroundImage: 'url("/banner.jpg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
 
-export default Home
+      <div className="searchButtonContainer">
+        <div className="shorterWidth">
+          <SearchButton />
+        </div>
+      </div>
+
+      <div className="class-section">
+        <Link href="/courses/statics" passHref>
+          <div className="class-button">
+            <img src="/class.jpg" alt="Statics" />
+            <p>Statics</p>
+          </div>
+        </Link>
+        <Link href="/courses/dynamics" passHref>
+          <div className="class-button">
+            <img src="/class.jpg" alt="Dynamics" />
+            <p>Dynamics</p>
+          </div>
+        </Link>
+        <Link href="/courses/strengths-of-materials" passHref>
+          <div className="class-button">
+            <img src="/class.jpg" alt="Strengths of Materials" />
+            <p>Strengths of Materials</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
