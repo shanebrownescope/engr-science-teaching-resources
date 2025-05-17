dee260b18299324a7eed00d0503736761cc1881e
GaIlades <fireman.huang38@gmail.com>
2025-05-16 22:18:54 -0700

    Inlcuded instructions in landing page

--- engr-site/components/mantine/HeroBullets.tsx
+++ engr-site/components/mantine/HeroBullets.tsx
@@ -9,8 +9,10 @@ import {
   List,
   ThemeIcon,
   rem,
+  SimpleGrid,
+  Box,
 } from "@mantine/core";
-import { IconCheck } from "@tabler/icons-react";
+import { IconCheck, IconSearch, IconNotebook, IconMessage, IconShare } from "@tabler/icons-react";
 import image from "./image.svg";
 import classes from "./HeroBullets.module.css";
 
@@ -20,6 +22,49 @@ import classes from "./HeroBullets.module.css";
  * @returns {JSX.Element} - The rendered HeroBullets component.
  */
 export function HeroBullets() {
+  const instructionData = [
+    {
+      icon: IconSearch,
+      title: 'Search for Resources',
+      description:
+        'Use the search bar above to find teaching materials by entering keywords related to your topic of interest.',
+    },
+    {
+      icon: IconNotebook,
+      title: 'Access Resources',
+      description:
+        'Select any resource to view details, download materials, and see related concepts and topics.',
+    },
+    {
+      icon: IconMessage,
+      title: 'Share Your Feedback',
+      description:
+        'Help the community by leaving reviews on resources you\'ve used in your teaching.',
+    },
+    {
+      icon: IconShare,
+      title: 'Contribute',
+      description:
+        'Have materials to share? Fill out our request form to contribute your own learning resources to the repository.',
+    },
+  ];
+
+  const instructionItems = instructionData.map((item, index) => (
+    <div key={index} className={classes.instructionStep}>
+      <ThemeIcon size={44} radius="md" className={classes.instructionIcon}>
+        <item.icon size={24} stroke={1.5} />
+      </ThemeIcon>
+      <Box ml="md">
+        <Text fw={500} fz="lg" mb={7}>
+          {index + 1}. {item.title}
+        </Text>
+        <Text c="dimmed" fz="sm">
+          {item.description}
+        </Text>
+      </Box>
+    </div>
+  ));
+
   return (
     <Container size="md">
       <div className={classes.inner}>
@@ -80,6 +125,17 @@ export function HeroBullets() {
           </Group>
         </div>
         <Image src={image.src} className={classes.image} />
+      </div>
+      
+      {/* Instructions Section */}
+      <div className={classes.instructionsContainer}>
+        <Title order={2} className={classes.instructionsTitle}>
+          How to Use E-SCoPe
+        </Title>
+        
+        <SimpleGrid cols={{ base: 1, md: 2 }} spacing={30} mt={30}>
+          {instructionItems}
+        </SimpleGrid>
       </div>
     </Container>
   );
